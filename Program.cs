4a470853e05e219cb8a58fa3defa6575b2844f63
Victor Nascimento Aguiar <112593695+victorNascimentoAguiar@users.noreply.github.com>
2024-01-11 19:13:16 -0300

    gerador de numeros aleatorios que não se repetem

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numeros = new int[6];
+Random random = new Random();
+random.Next();
+int aleatorio;
+
+for(int c =0; c < 6; c++)
+{
+   aleatorio = random.Next(1, 61);
+   while(numeros.Contains (aleatorio))
+   {
+    aleatorio = random.Next(1,61);
+   }
+   numeros[c] = aleatorio;
+
+   Console.Write(numeros[c] + " " );
+   
+}
